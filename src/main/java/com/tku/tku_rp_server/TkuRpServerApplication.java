a0f56dacb69151e813b8adefa9c11e66f6f07aea
chouxiaozi1989 <caiyi1989@qq.com>
2020-05-15 17:29:42 +0800

    init

---
+++ src/main/java/com/tku/tku_rp_server/TkuRpServerApplication.java
@@ -0,0 +1,13 @@
+package com.tku.tku_rp_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TkuRpServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TkuRpServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tku/tku_rp_server/TkuRpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tku.tku_rp_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TkuRpServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
